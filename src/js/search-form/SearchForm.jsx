cd61cf40835a9b82f94bba6bd0a1661c32f9a9dd
Adam Varga <adam@secretsaucepartners.com>
2017-03-14 15:35:26 +0100

    Change search input placeholder text

--- src/js/search-form/SearchForm.jsx
+++ src/js/search-form/SearchForm.jsx
@@ -4,7 +4,7 @@ export default class SearchForm extends React.Component {
   render() {
     return <div className="search-form">
                <div className="filter">
-                 <input type="text" id="search-filter" placeholder='Filter...'/>
+                 <input type="text" id="search-filter" placeholder='Enter a filter query...'/>
                </div>
                <button id="search"></button>
            </div>;
